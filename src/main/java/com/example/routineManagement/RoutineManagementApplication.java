a89340b2dc0eb271848cc06915754d1820f32c51
0xc0de1dea <0xc0de1dea@gmail.com>
2026-01-09 17:26:53 +0900

    init

---
+++ src/main/java/com/example/routineManagement/RoutineManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.routineManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutineManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutineManagementApplication.class, args);
+	}
+
+}
